9a774fc22e236cb09ae78850c7a3615636fdd9c4
Highsky7 <albert31115@gmail.com>
2024-08-11 13:10:26 +0900

    Update doldol_ros.ino
    change STEER_OFFSET,MAX_STEER_TIRE_DEG, KP, and Mapping(steering_val)

--- arduino/doldol_ros/doldol_ros.ino
+++ arduino/doldol_ros/doldol_ros.ino
@@ -23,13 +23,13 @@
 #define CH4_PULSE_PIN           20
 
 #define ACCEL_OFFSET            0.07
-#define STEER_OFFSET            0.13
+#define STEER_OFFSET            0.0
 #define SIGNAL_THRESHOLD        0.1
        
 #define STEER_DETECT_MAX        410
-#define STEER_DETECT_MIN        772
-#define MAX_STEER_TIRE_DEG      19.85
-#define KP                      0.17
+#define STEER_DETECT_MIN        749
+#define MAX_STEER_TIRE_DEG      20.09
+#define KP                      0.2
 #define KI                      0.0
 #define KD                      0.0
 
@@ -269,7 +269,7 @@ void loop() {
 
 
   float throttle_input = Mapping(accel_val, 1000.0, 2000.0, -1.0, 1.0);
-  float steer_input = Mapping(steering_val, 1000.0, 2000.0, -1.0, 1.0); 
+  float steer_input = Mapping(steering_val, 1224.0, 1736.0, -1.0, 1.0); 
 
   // Serial.print(throttle_input);
   // Serial.print("\t");
